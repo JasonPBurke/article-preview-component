c538f02132dc12bc12af19cc4d522afdba091ac2
Jason Burke <jpburke77@gmail.com>
2025-03-05 02:12:05 -0600

    javascript popup action applied

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-const test = document.getElementById('test');
-console.log(test);
+const popup = document.getElementById('share-popup');
+const shareButton = document.getElementById('share-button');
+const mobileShareClose = document.getElementById('mobile-share-button');
+
+shareButton.addEventListener('click', (e) => {
+  if (!popup.style.display || popup.style.display === 'none') {
+    popup.style.display = 'flex';
+  } else {
+    popup.style.display = 'none';
+  }
+});
+
+mobileShareClose.addEventListener('click', (e) => {
+  popup.style.display = 'none';
+});
