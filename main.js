fa271c9d180fc5da4d57429d555fdee8e5bfceb6
Jason Burke <jpburke77@gmail.com>
2025-03-05 02:36:24 -0600

    minor style fixes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const popup = document.getElementById('share-popup');
 const shareButton = document.getElementById('share-button');
 const mobileShareClose = document.getElementById('mobile-share-button');
+const author = document.getElementById('author');
 
 shareButton.addEventListener('click', (e) => {
   if (!popup.style.display || popup.style.display === 'none') {
@@ -8,8 +9,13 @@ shareButton.addEventListener('click', (e) => {
   } else {
     popup.style.display = 'none';
   }
+  // let cardWidth = document.getElementById('card').offsetWidth;
+  // if (cardWidth <= 376) {
+  //   author.style.display = 'none';
+  // }
 });
 
 mobileShareClose.addEventListener('click', (e) => {
   popup.style.display = 'none';
+  author.style.display = 'flex';
 });
